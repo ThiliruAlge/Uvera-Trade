9b7501d53bf62ee612da791acd5083eec25bd36c
ThiliruAlge <thiliru.sanjula@mail.com>
2022-10-17 17:35:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/uveratrade/ModelVideoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.uveratrade
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModelVideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_model_video)
+
+        //supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+}
---
+++ app/src/main/java/com/example/uveratrade/PropertyDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uveratrade
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PropertyDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_property_details)
+    }
+}
